983e9aea984dc7ced616a334a76c54f24da85399
Jed Northridge <jedcn@jedcn.com>
2014-08-15 16:00:52 -0400

    [jasmine] Added more to suiteSpecs.js

--- jasmine/test/suiteSpec.js
+++ jasmine/test/suiteSpec.js
@@ -10,3 +10,20 @@ describe("A suite is just a function", function() {
     expect(a).toBe(false);
   });
 });
+
+describe("A spec", function() {
+  it("is just a function, so it can contain any code", function() {
+    var foo = 0;
+    foo += 1;
+
+    expect(foo).toEqual(1);
+  });
+
+  it("can have more than one expectation", function() {
+    var foo = 0;
+    foo += 1;
+
+    expect(foo).toEqual(1);
+    expect(true).toEqual(true);
+  });
+});
